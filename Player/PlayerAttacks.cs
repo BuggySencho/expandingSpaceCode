6d0181b6bf9d594345935516c81b1a8789040a31
JustinvdLans <42778559+JustinvdLans@users.noreply.github.com>
2019-05-16 11:43:03 +0200

    Add files via upload

---
+++ Player/PlayerAttacks.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAttacks : MonoBehaviour {
+	// create empty and call it AttackPos, bit in front of character.
+	// put it in attackPos in character and make it a child of character.
+	// in whatIsEnemies select enemy.
+	// give enemy an enemy layer first.
+	
+	GameObject Enemy; 
+	public EnemyDamage enemyBehaviourScript;
+
+	private float timeBtwAttack;
+	public float startTimeBtwAttack;
+	
+	public int health;
+	public Transform attackPos;
+	public LayerMask whatIsEnemies;
+	public float attackRange;
+	public int damage;
+
+	void start()
+	{
+		Enemy = GameObject.Find("Enemy");
+        enemyBehaviourScript = Enemy.GetComponent<EnemyDamage>();
+	}
+
+	void Update () 
+	{
+		if(timeBtwAttack <= 0)
+		{
+			if(Input.GetKey(KeyCode.Space))
+			{
+				Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+				for(int i = 0; i < enemiesToDamage.Length; i++) 
+				{
+					enemiesToDamage[i].GetComponent<EnemyDamage>().TakeDamage(damage);
+				}
+			}
+		timeBtwAttack = startTimeBtwAttack;
+		} else {
+			timeBtwAttack -= Time.deltaTime; 
+		}
+	}	
+	public void OnDrawGizmosSelected()
+		{	
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere(attackPos.position, attackRange);
+		}
+}
---
+++ Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float dashSpeed;
+	private float dashTime;
+	public float startDashTime;
+	private int direction;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		dashTime = startDashTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(direction == 0)
+		{
+			if(Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				direction = 1;
+			} else if(Input.GetKeyDown(KeyCode.RightArrow)){
+				direction = 2;
+			} 
+		} else {
+				if(dashTime <= 0)
+				{
+					direction = 0;
+					dashTime = startDashTime;
+					rb.velocity = Vector2.zero;
+				} else {
+					dashTime -= Time.deltaTime;
+				}
+
+				if(direction == 1) {
+					rb.velocity = Vector2.left * dashSpeed;
+				} else if (direction == 2) {
+					rb.velocity = Vector2.right * dashSpeed;
+				}
+		}
+	}
+}
